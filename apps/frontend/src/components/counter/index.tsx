b239e183af75e35edccc8c66d7c88b3b212fda51
Roman H. <info@romischart.de>
2024-02-18 23:45:44 +0100

    TASK: cleanup counter component

--- apps/frontend/src/components/counter/index.tsx
+++ apps/frontend/src/components/counter/index.tsx
@@ -2,15 +2,15 @@
 
 import { Button } from '@nextui-org/button';
 import { useAppDispatch, useAppSelector } from '@/store';
-import { increment } from '@/store/features/counter/counter.slice';
+import { increment, selectCount } from '@/store/features/counter/counter.slice';
 
 export function Counter() {
-  const { value } = useAppSelector((state) => state.counter);
+  const count = useAppSelector(selectCount);
   const dispatch = useAppDispatch();
 
   return (
     <Button onPress={() => dispatch(increment())} radius="full">
-      Count is {value}
+      Count is {count}
     </Button>
   );
 }
